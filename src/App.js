47048e79b5bac5e97dd151c9e393289d94b4a968
pedroSK <p.hlavacik@gmail.com>
2021-02-02 12:21:39 +0100

    git init & netlify deploy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Basic from "./Basic";
+import Airtable from "./Airtable";
+
+function App() {
+  return (
+    <>
+      <Basic />
+      <Airtable />
+    </>
+  );
+}
+
+export default App;
